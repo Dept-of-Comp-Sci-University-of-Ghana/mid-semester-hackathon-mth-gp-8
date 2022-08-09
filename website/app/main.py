340a026f87a825d748ddab83854e8a241323ccfd
10829272 <Cabrokwa001@st.ug.edu.gh>
2022-08-09 06:17:15 +0000

    Refactored code

--- website/app/main.py
+++ website/app/main.py
@@ -1,16 +1,14 @@
+import pickle
+from typing import Optional
+
 from fastapi import APIRouter, FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from app.routers import result
-
-from .library.helpers import openfile
+from .library.helpers import openfile, text_cleaning
 
 app = FastAPI()
-router = APIRouter()
-
-app.include_router(result.router)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -31,3 +29,27 @@ async def show_page(request: Request, page_name: str):
     return templates.TemplateResponse(
         "page.html", {"request": request, "data": data}
     )
+
+@app.get("/form1", response_class=HTMLResponse)
+def flag_text(request: Request, query: Optional[str] = None):
+	"""
+    A simple function that receive a query and predicts the insult potential of the content.
+    :param review:
+    :return: prediction
+    """
+	model = pickle.load(open("../model/saved_model.pkl", 'rb'))
+    
+    # clean the review
+	cleaned_query = text_cleaning(query)
+    
+    # perform prediction
+	prediction = model.predict([cleaned_query])
+	output = int(prediction[0])
+
+    # output dictionary
+	result = {0: "Not an insult", 1: "Insult"}
+    
+    # show results
+	data = openfile("home.md")
+	return templates.TemplateResponse("page.html", context={"request":request, "data":data, "result": result[output], "OriginalUserInput":query, "UserInput":cleaned_query})
+

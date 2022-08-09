89cb7ae8b88429b1e188686e0b97cfce77373025
10829272 <Cabrokwa001@st.ug.edu.gh>
2022-08-09 05:26:56 +0000

    Updated model to reflect new notebook changes

--- website/app/library/helpers.py
+++ website/app/library/helpers.py
@@ -1,5 +1,15 @@
+import os
 import os.path
+import re  # regular expression
+
+# text preprocessing modules
+from string import punctuation
+
 import markdown
+
+# text preprocessing modules
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 
 def openfile(filename):
@@ -12,3 +22,28 @@ def openfile(filename):
         "text": html
     }
     return data
+
+def text_cleaning(text, remove_stop_words=True, lemmatize_words=True):
+    # Clean the text, with the option to remove stop_words and to lemmatize word
+    # Clean the text
+    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"http\S+", " link ", text)
+    text = re.sub(r"\b\d+(?:\.\d+)?\s+", "", text)  # remove numbers
+    # Remove punctuation from text
+    text = "".join([c for c in text if c not in punctuation])
+    # Optionally, remove stop words
+    if remove_stop_words:
+        # load stopwords
+        stop_words = stopwords.words("english")
+        text = text.split()
+        text = [w for w in text if not w in stop_words]
+        text = " ".join(text)
+    # Optionally, shorten words to their stems
+    if lemmatize_words:
+        text = text.split()
+        lemmatizer = WordNetLemmatizer()
+        lemmatized_words = [lemmatizer.lemmatize(word) for word in text]
+        text = " ".join(lemmatized_words)
+    # Return a list of words
+    return text
--- website/app/routers/result.py
+++ website/app/routers/result.py
@@ -5,18 +5,30 @@ from fastapi import APIRouter, FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from ..library.helpers import text_cleaning
+
 router = APIRouter()
 templates = Jinja2Templates(directory="templates/")
 
-
-# @router.get("/form1", response_class=HTMLResponse)
-# def form_get(request: Request):
-# 	result = "Enter your text"
-# 	return templates.TemplateResponse("result.html", context={"request":request, "result": result})
-
-
 @router.get("/form1", response_class=HTMLResponse)
-def form_post1(request: Request, query: Optional[str] = None):
-	loaded_model = pickle.load(open("../model/saved_model.pkl", 'rb'))
-	out = loaded_model.predict(query)
-	return templates.TemplateResponse("result.html", context={"request":request, "result": out, "yourtext": query})
+def flagg_text(request: Request, query: Optional[str] = None):
+	"""
+    A simple function that receive a query and predicts the insult potential of the content.
+    :param review:
+    :return: prediction
+    """
+	model = pickle.load(open("../model/saved_model.pkl", 'rb'))
+    
+    # clean the review
+	cleaned_query = text_cleaning(query)
+    
+    # perform prediction
+	prediction = model.predict([cleaned_query])
+	output = int(prediction[0])
+
+    # output dictionary
+	result = {0: "Not an insult", 1: "Insult"}
+    
+    # show results
+	return templates.TemplateResponse("result.html", context={"request":request, "result": result[output], "yourtext":query})
+
